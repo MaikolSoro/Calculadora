89e379e8332b808b7e32dc211873217dc2ad75aa
Maikol Soro <maikolsoro.z1998@gmail.com>
2022-05-20 00:13:09 -0600

    Created a class with the respective operations of the calculator separately

---
+++ Operaciones.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Operaciones
+    {
+
+        /// <summary>
+        /// Suma dos numeros
+        /// </summary>
+        /// <param name="n1" name="n2">Tamaño del arreglo</param>
+        /// <returns>resultado de la suma numero entero</returns>
+
+        internal int Sumar(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        /// <summary>
+        /// Suma dos numeros
+        /// </summary>
+        /// <param name="n1" name="n2">Tamaño del arreglo</param>
+        /// <returns>resultado de la resta, numero entero</returns>
+        internal int Restar(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        /// <summary>
+        /// Suma dos numeros
+        /// </summary>
+        /// <param name="n1" name="n2">Tamaño del arreglo</param>
+        /// <returns>resultado de la multiplación, numero entero</returns>
+        internal int Multiplicar(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        /// <summary>
+        /// Suma dos numeros
+        /// </summary>
+        /// <param name="n1" name="n2">Tamaño del arreglo</param>
+        /// <returns>resultado de la division, numero entero</returns>
+        internal int Dividir(int n1, int n2)
+        {
+            if (n2 == 0)
+            {
+                return 0;
+            }
+            return n1 / n2;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,99 +12,92 @@ namespace Calculadora
         static void Main(string[] args)
 
         {
-            string resp = "";
+            string menu = "\nMenu Calculadora \n" +
+               "1. Sumar\n" +
+               "2. Restar\n" +
+               "3. Multiplicar\n" +
+               "4. Dividir\n" +
+               "5. Salir\n" +
+               "Seleccione una opción: ";
 
-            do
+            bool salir = false;
+            Operaciones ope = new Operaciones();
 
+            while (!salir)
             {
                 Console.Clear();
+                Console.Write(menu);
+                int op = Int32.Parse(Console.ReadLine());
+               
 
-                int numero1 = 0; //Aqui se almacenara el primer valor ingresado.
-
-                int numero2 = 0; //Aqui se almacenara el segundo valor ingresado.
-
-                int resultado = 0; //Aqui se almacenara el resultado de la operacion.
-
-
-
-                //Aqui mostraremos los mensajes que apareceran en nuestra consola igual que el menu de seleccion.
-                Console.WriteLine("Presione el simbolo para realizar la operacion:\n");
-
-                Console.WriteLine("+ para sumar\n");
-
-                Console.WriteLine("- para restar\n");
-
-                Console.WriteLine("* para multiplicar\n");
-
-                Console.WriteLine("/ para dividir\n");
-
-                Console.Write("\nEliga Una Opcion: "); //Aqui es donde indicaremos que operacion vamos a realizar
-
-                resp = Console.ReadLine();
-
-                string eleccion = Convert.ToString(resp);
-
-                //Aqui se agregaran los mensajes para poder ingresar los datos.
-
-                Console.WriteLine("\nIngrese sus dos numerosn");
-
-                Console.Write("Numero 1: ");
-                numero1 = int.Parse(Console.ReadLine());
-
-                Console.Write("Numero 2: ");
-
-                numero2 = int.Parse(Console.ReadLine());
-
-                Console.WriteLine();
-
-                switch (eleccion)
+                switch (op)
                 {
-                    case "+":
+                    case 1:
+                        Console.Write("Numero 1: ");
+                        int numero1 = int.Parse(Console.ReadLine());
 
-                        Console.WriteLine("El resultado de la suma es:");
-                        resultado = numero1 + numero2;
+                        Console.Write("Numero 2: ");
 
-                        Console.WriteLine("{0} + {1} = {2}", numero1, numero2, resultado);
+                        int numero2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+
+                        int res = ope.Sumar(numero1, numero2);
+                        Console.WriteLine("{0}+{1}={2}", numero1, numero2, res);
                         break;
+                    case 2:
+                        Console.Write("Numero 1: ");
+                         numero1 = int.Parse(Console.ReadLine());
 
-                    case "-":
+                        Console.Write("Numero 2: ");
 
-                        Console.WriteLine("El resultado de la resta es:");
+                         numero2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
 
-                        resultado = numero1 - numero2;
-
-                        Console.WriteLine("{0} - {1} = {2}", numero1, numero2, resultado);
-
+                        res = ope.Restar(numero1, numero2);
+                        Console.WriteLine("{0}-{1}={2}", numero1, numero2, res);
                         break;
-                    case "*":
+                    case 3:
+                        Console.Write("Numero 1: ");
+                         numero1 = int.Parse(Console.ReadLine());
 
-                        Console.WriteLine("El resultado de la multiplicacion es:");
-                        resultado = numero1 * numero2;
+                        Console.Write("Numero 2: ");
 
-                        Console.WriteLine("{0} * {1} = {2}", numero1, numero2, resultado);
+                         numero2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
 
+                        res = ope.Multiplicar(numero1, numero2);
+                        Console.WriteLine("{0}*{1}={2}", numero1, numero2, res);
                         break;
-                    case "/":
-                        Console.WriteLine("El resultado de la division es:");
+                    case 4:
+                        Console.Write("Numero 1: ");
+                         numero1 = int.Parse(Console.ReadLine());
 
-                        resultado = numero1 / numero2;
+                        Console.Write("Numero 2: ");
 
-                        Console.WriteLine("{0} / {1} = {2}", numero1, numero2, resultado);
+                         numero2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
 
+                        res = ope.Dividir(numero1, numero2);
+                        Console.WriteLine("{0}/{1}={2}", numero1, numero2, res);
                         break;
-
+                    case 5:
+                        Console.WriteLine("Salir");
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opción Inválida");
+                        break;
                 }
-                Console.Write("\n¿Desea Continuar? s/n: "); //Si para realizarlo de nuevo y no para salir.
 
-                resp = Console.ReadLine();
-
+                Console.ReadKey();
             }
-            while (resp == "s" || resp == "s");
-
         }
+
 
     }
 }
+
+
 
 
     
